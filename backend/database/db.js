1d0b540fdd4ee5c5c86d0cc4219df3471c5e2913
shoyos100 <shoyos100@hotmail.com>
2022-11-19 09:48:47 -0500

    ultimo

---
+++ backend/database/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    mongoURI:
+    "mongodb+srv://santiago1:santiago19907@cluster0.3pt5wcp.mongodb.net/reactdb?retryWrites=true&w=majority",
+
+    secretOrkey:"seret",
+
+
+};
